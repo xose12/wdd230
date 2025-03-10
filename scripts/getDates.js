fc81f299917856af4647f6084e55db4a916b0afa
xose12 <xosekarikari@gmail.com>
2025-03-09 23:21:36 -0700

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,9 @@
-// Dynamically set the current year in the footer
-const yearElement = document.getElementById('year');
-yearElement.textContent = new Date().getFullYear();
-
-// Dynamically set the last modified date
-const lastModifiedElement = document.getElementById('lastModified');
-lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
+let year = document.querySelector("#year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+//Remember that to get the Id element you should have use getelementbyid not queryselector, as the instructions said
+let modify = document.querySelector("#lastmodified")
+//There was no need to use new Date, if you delete it you'll see it'll work the same
+const currentTime = new Date(document.lastModified)
+//The lastModified property of the Document interface returns a string containing the date and time on which the current document was last modified.
+modify.textContent = document.lastModified
